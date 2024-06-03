e9263b4d21c8a850723f1e2a09b2f12fb39b10b3
rstlix0x0 <rstlix.dev@gmail.com>
2024-06-03 23:32:26 +0700

    refactor(core): adding decryption method to private key pairs

--- core/src/keys/secure.rs
+++ core/src/keys/secure.rs
@@ -1,17 +1,28 @@
 use rst_common::standard::serde::{self, Deserialize, Serialize};
 use rst_common::standard::serde_json;
+use rst_common::with_errors::thiserror::{self, Error as ThisError};
 
 use prople_crypto::keysecure::KeySecure;
 
 use crate::types::{DIDError, ToJSON};
 
 /// `Error` defined for specific this module error types
-#[derive(Debug)]
+#[derive(Debug, ThisError)]
 pub enum Error {
+    #[error("keysecure error")]
     BuildKeySecureError,
+
+    #[error("keysecure error: unable to build identity private keys")]
     BuildIdentityPrivateKeysError,
+
+    #[error("keysecure error: unable to build JSON")]
     BuildJSONError,
+
+    #[error("keysecure error: storage error")]
     StorageError,
+
+    #[error("keysecure error: decrypt error: {0}")]
+    DecryptError(String),
 }
 
 /// `KeySecureBuilder` is a trait that should be implemented by any objects
@@ -31,6 +42,26 @@ pub trait IdentityPrivateKeyPairsBuilder {
 pub struct PrivateKeyPairs {
     pub verification: KeySecure,
     pub aggrement: KeySecure,
+}
+
+impl PrivateKeyPairs {
+    pub fn decrypt_verification(&self, password: String) -> Result<Vec<u8>, Error> {
+        let decrypted = self
+            .verification
+            .decrypt(password)
+            .map_err(|err| Error::DecryptError(err.to_string()))?;
+
+        Ok(decrypted)
+    }
+
+    pub fn decrypt_agreement(&self, password: String) -> Result<Vec<u8>, Error> {
+        let decrypted = self
+            .aggrement
+            .decrypt(password)
+            .map_err(|err| Error::DecryptError(err.to_string()))?;
+
+        Ok(decrypted)
+    }
 }
 
 /// `IdentityPrivateKeyPairs` used to specific identity object, it is different with the `Identity` object
